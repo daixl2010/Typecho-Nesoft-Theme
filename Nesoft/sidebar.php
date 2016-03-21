2323d9747d927d60ee67d9b3e1a910fe47c434ce
daixl2010 <6103923@qq.com>
2016-03-21 10:55:33 +0800

    侧边栏最新文章图文展示

--- Nesoft/sidebar.php
+++ Nesoft/sidebar.php
@@ -1,10 +1,17 @@
-<section class="widget panel">
-	<h3 class="widget-title"><i class="fa"></i><?php _e('最新文章'); ?></h3>
-	<ul class="widget-list">
-		<?php $this->widget('Widget_Contents_Post_Recent')
-		->parse('<li><a href="{permalink}">{title}</a></li>'); ?>
+<aside id="widget_ui_together" class="widget widget_ui_together panel clearfix">
+	<div class="widget-title"><i class="fa"></i>最新文章</div>
+	<ul>			
+		<?php $this->widget('Widget_Contents_Post_Recent','pageSize=3')->to($posts); ?>
+		<?php while($posts->next()): ?>
+			<li><a href="<?php $posts->permalink() ?>" title="<?php $posts->title() ?>">
+				<img src="<?php $posts->options->themeUrl(); ?>timthumb.php?src=<?php Thumbnail_Plugin::show($posts); ?>&h=150&w=310&zc=1" />
+				<span class="text"><?php $posts->title() ?></span>
+				<span class="wid-tg-comment"><i class="fa fa-comments-o"></i> <?php $posts->commentsNum('%d 评论'); ?></span>
+			</a></li>
+		<?php endwhile; ?>		
 	</ul>
-</section>
+</aside>
+
 <?php $this->need('inc/ad/ad_widget_top.php'); ?>
 <section class="widget panel">
 	<h3 class="widget-title"><i class="fa"></i><?php _e('归档'); ?></h3>
